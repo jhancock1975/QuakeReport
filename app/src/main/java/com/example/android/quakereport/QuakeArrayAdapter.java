207de0d94bf59d243e6fb13b4a01c19bb308e613
John Hancock <jhancock1975@gmail.com>
2018-01-28 20:52:24 -0500

    add styling, date format

--- app/src/main/java/com/example/android/quakereport/QuakeArrayAdapter.java
+++ app/src/main/java/com/example/android/quakereport/QuakeArrayAdapter.java
@@ -1,13 +1,11 @@
 package com.example.android.quakereport;
 
 import android.app.Activity;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.text.DateFormat;
@@ -24,45 +22,70 @@ class QuakeArrayAdapter extends ArrayAdapter<QuakeListItem> {
         super(context, 0, earthquakes);
     }
 
-    private static DateFormat listItemDateFormat = new SimpleDateFormat("MMM dd, yyyy");
+    private static DateFormat listItemDateFormat = new SimpleDateFormat("MMM dd, yyyy ");
+    private static DateFormat listItemTimeFormat = new SimpleDateFormat("HH:mm:ss a");
+    public static final String LOG_TAG = QuakeArrayAdapter.class.getName();
 
     /**
      * Provides a view for an AdapterView (ListView, GridView, etc.)
      *
-     * @param position The position in the list of data that should be displayed in the
-     *                 list item view.
+     * @param position    The position in the list of data that should be displayed in the
+     *                    list item view.
      * @param convertView The recycled view to populate.
-     * @param parent The parent ViewGroup that is used for inflation.
+     * @param parent      The parent ViewGroup that is used for inflation.
      * @return The View for the position in the AdapterView.
      */
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         // Check if the existing view is being reused, otherwise inflate the view
         View listItemView = convertView;
-        if(listItemView == null) {
+        if (listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(
                     R.layout.quake_list_item, parent, false);
         }
 
-        // Get the {@link AndroidFlavor} object located at this position in the list
+
         QuakeListItem quakeListItem = getItem(position);
 
-        // Find the TextView in the list_item.xml layout with the ID version_name
+        // Find the TextViews and set their text
         TextView magnitudeTextView = (TextView) listItemView.findViewById(R.id.quake_list_magnitude);
-        // Get the version name from the current AndroidFlavor object and
-        // set this text on the name TextView
-        magnitudeTextView.setText(quakeListItem.getMagnitude()+"");
 
-        // Find the TextView in the list_item.xml layout with the ID version_number
-        TextView locationTextView = (TextView) listItemView.findViewById(R.id.quake_list_lication);
-        // Get the version number from the current AndroidFlavor object and
-        // set this text on the number TextView
-        locationTextView.setText(quakeListItem.getLocation());
+        magnitudeTextView.setText(quakeListItem.getMagnitude() + "");
+
+
+        TextView locationDirectionTextView =
+                (TextView) listItemView.findViewById(R.id.quake_list_location_direction);
+        TextView locationPlaceTextView =
+                (TextView) listItemView.findViewById(R.id.quake_list_location_place_name);
+
+
+        try {
+            String[] locationSplit = quakeListItem.getLocation()
+                    .split(getContext().getString(R.string.location_seperator));
+
+            for (String s: locationSplit){
+                Log.d(LOG_TAG, "QuakeArrayAdapter::getView location" +
+                        "split element = " + s + ".");
+
+            }
+            Log.d(LOG_TAG, "***");
+
+
+            locationDirectionTextView.setText(locationSplit[0]
+                    + getContext().getString(R.string.location_seperator));
+            locationPlaceTextView.setText(locationSplit[1]);
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            locationDirectionTextView.setText(quakeListItem.getLocation());
+        }
 
         TextView dateTextView = (TextView) listItemView.findViewById(R.id.quake_list_date);
         dateTextView.setText(listItemDateFormat.format(quakeListItem.getDate()));
 
-        // Return the whole list item layout (containing 2 TextViews and an ImageView)
+        TextView timeTextView = (TextView) listItemView.findViewById(R.id.quake_list_time);
+        timeTextView.setText(listItemTimeFormat.format(quakeListItem.getDate()));
+
+        // Return the whole list item layout
         // so that it can be shown in the ListView
         return listItemView;
     }
