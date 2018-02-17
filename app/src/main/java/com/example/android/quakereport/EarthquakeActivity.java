555e6713dd8beb445f08b67f3f66765d7deab0de
John Hancock <jhancock1975@gmail.com>
2018-02-17 16:00:45 -0500

    add max results setting

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -17,12 +17,18 @@ package com.example.android.quakereport;
 
 import android.app.LoaderManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.ProgressBar;
@@ -35,10 +41,10 @@ public class EarthquakeActivity extends AppCompatActivity
         implements LoaderManager.LoaderCallbacks<List<QuakeListItem>> {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
-    public final static String baseUrl = "https://earthquake.usgs.gov";
-    private static final String path = "/fdsnws/event/1/";
-    private static final String query
-            = "query?format=geojson&eventtype=earthquake&orderby=time&limit=10";
+
+    public static final String BASE_URL = "https://earthquake.usgs.gov";
+    public static final String PATH = "/fdsnws/event/1/query";
+
     private QuakeArrayAdapter adapter;
     private ListView earthquakeListView;
 
@@ -97,7 +103,36 @@ public class EarthquakeActivity extends AppCompatActivity
     @Override
     public Loader<List<QuakeListItem>> onCreateLoader(int i, Bundle bundle) {
         Log.d(EarthquakeActivity.LOG_TAG, "oncreateLoader::begin");
-        return new EarthquakeLoader(this, baseUrl + path + query);
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        String minMagnitude = sharedPrefs.getString(
+                getString(R.string.settings_min_magnitude_key),
+                getString(R.string.settings_min_magnitude_default));
+
+
+
+        String orderBy = sharedPrefs.getString(
+                getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default)
+        );
+
+        String maxResults = sharedPrefs.getString(
+                getString(R.string.settings_max_results_key),
+                getString(R.string.settings_max_results_default));
+
+
+
+        Uri baseUri = Uri.parse(EarthquakeActivity.BASE_URL + PATH);
+
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+        uriBuilder.appendQueryParameter("format", "geojson");
+        uriBuilder.appendQueryParameter("limit", maxResults);
+        uriBuilder.appendQueryParameter("minmag", minMagnitude);
+        uriBuilder.appendQueryParameter("orderby", orderBy);
+
+
+        return new EarthquakeLoader(this, uriBuilder.toString());
     }
 
     @Override
@@ -132,5 +167,22 @@ public class EarthquakeActivity extends AppCompatActivity
     public void onLoaderReset(Loader<List<QuakeListItem>> loader) {
         Log.d(EarthquakeActivity.LOG_TAG, "onLoaderRest begin");
         adapter.setEarthquakes(QueryUtils.getInitialList());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
